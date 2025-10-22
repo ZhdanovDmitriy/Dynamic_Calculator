b7f9c980e1c9710338ea2353cc1dabb1873f7ff5
Дмитрий Жданов <D.Zhdanov66@ya.ru>
2025-10-23 01:07:31 +0300

    refactor: изменено тестирование(тесты не зависят от имен файлов и их количества)

--- tests.cpp
+++ tests.cpp
@@ -3,70 +3,70 @@
 #include <string>
 #include <vector>
 #include <cmath>
-#include <cassert>
+#include <filesystem>
 
 typedef double(*FuncPtr)(double, double);
-
-struct TestCase {
-    double a;
-    double b;
-    double expectedResult;
-    bool expectException;
-};
-
-struct Test {
-    std::string name;
-    std::vector<TestCase> cases;
-};
 
 double callFunc(FuncPtr func, double a, double b) {
     return func(a, b);
 }
 
 void runTests() {
-    std::vector<std::string> pluginFiles = {
-        "plugins/funcAdd.dll",
-        "plugins/funcSub.dll",
-        "plugins/funcMul.dll",
-        "plugins/funcDiv.dll",
-        "plugins/funcPow.dll",
-        "plugins/funcSqrt.dll",
-        "plugins/funcSin.dll",
-        "plugins/funcCos.dll",
-        "plugins/funcLn.dll",
-        "plugins/funcTg.dll",
-        "plugins/funcCtg.dll",
-        "plugins/funcMod.dll",
-        "plugins/funcDivInt.dll"
-    };
+#ifdef _DEBUG
+    std::cout << "Текущий каталог: " << std::filesystem::current_path() << std::endl;
+#endif
+
+#ifdef _DEBUG
+    std::filesystem::path pluginsDir = "plugins/debug";
+#else
+    std::filesystem::path pluginsDir = "plugins/release";
+#endif
 
 #ifdef _DEBUG
     std::cout << "Юнит тестирование:" << std::endl;
 #endif
 
-    for (size_t i = 0; i < pluginFiles.size(); ++i) {
-        HMODULE hModule = LoadLibraryA(pluginFiles[i].c_str());
-        if (!hModule) {
-#ifdef _DEBUG
-            std::cerr << "Ошибка: не удалось загрузить " << pluginFiles[i] << std::endl;
-#else
-            std::cerr << "Ошибка загрузки DLL" << std::endl;
-#endif
-            std::exit(EXIT_FAILURE);
-        }
+    bool allPluginsOk = true;
 
-        FuncPtr func = (FuncPtr)GetProcAddress(hModule, "func");
-        if (!func) {
+    for (const auto& entry : std::filesystem::directory_iterator(pluginsDir)) {
+        if (entry.is_regular_file() && entry.path().extension() == ".dll") {
+            const auto& pluginPath = entry.path();
 #ifdef _DEBUG
-            std::cerr << "Ошибка: функция 'func' не найдена в " << pluginFiles[i] << std::endl;
-#else
-            std::cerr << "Ошибка подключения функции DLL" << std::endl;
+            std::cout << "Проверяется DLL: " << pluginPath << std::endl;
 #endif
+
+            HMODULE hModule = LoadLibraryW(pluginPath.wstring().c_str());
+            if (!hModule) {
+#ifdef _DEBUG
+                std::cerr << "Ошибка: не удалось загрузить " << pluginPath << std::endl;
+#else
+                std::cerr << "Ошибка загрузки DLL" << std::endl;
+#endif
+                allPluginsOk = false;
+                continue;
+            }
+
+            FuncPtr func = reinterpret_cast<FuncPtr>(GetProcAddress(hModule, "func"));
+            if (!func) {
+#ifdef _DEBUG
+                std::cerr << "Ошибка: функция 'func' не найдена в " << pluginPath << std::endl;
+#else
+                std::cerr << "Ошибка подключения функции DLL" << std::endl;
+#endif
+                FreeLibrary(hModule);
+                allPluginsOk = false;
+                continue;
+            }
+
             FreeLibrary(hModule);
-            continue;
         }
-        FreeLibrary(hModule);
     }
 
-    std::cout << "Все динамические библиотеки успешно подключены." << std::endl;
+    if (allPluginsOk) {
+        std::cout << "Все динамические библиотеки валидны." << std::endl;
+    }
+    else {
+        std::cerr << "Не все динамические библиотеки валидны! Приложение будет завершено." << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
 }
